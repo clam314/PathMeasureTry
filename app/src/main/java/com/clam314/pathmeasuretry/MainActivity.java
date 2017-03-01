287f7e0709c62a1214bf57f03ab3719fcea3904d
wuweimin <clam314@163.com>
2017-03-01 15:22:11 +0800

    增加使用setpolytopoly的demo

--- app/src/main/java/com/clam314/pathmeasuretry/MainActivity.java
+++ app/src/main/java/com/clam314/pathmeasuretry/MainActivity.java
@@ -1,13 +1,30 @@
 package com.clam314.pathmeasuretry;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private PolygonView poly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_poly);
+        poly = (PolygonView) findViewById(R.id.poly);
+
+        RadioGroup group = (RadioGroup) findViewById(R.id.group);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()){
+                    case R.id.point0: poly.setTestPoint(0); break;
+                    case R.id.point1: poly.setTestPoint(1); break;
+                    case R.id.point2: poly.setTestPoint(2); break;
+                    case R.id.point3: poly.setTestPoint(3); break;
+                    case R.id.point4: poly.setTestPoint(4); break;
+                }
+            }
+        });
     }
 }
